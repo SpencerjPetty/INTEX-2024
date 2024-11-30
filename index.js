15c92885555d1b75749ec5126b7c0761f945fe3b
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2024-11-30 13:07:13 -0700

    added ssl

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const knex = require("knex") ({ // Connecting to our Postgres Database
         user : process.env.RDS_USERNAME || "postgres",
         password : process.env.RDS_PASSWORD || "Roman$EatLargeT0gas", // This would need to change
         database : process.env.RDS_DB_NAME || "intex",
-        port : process.env.RDS_PORT || 5432
+        port : process.env.RDS_PORT || 5432,
+        ssl : process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
 
