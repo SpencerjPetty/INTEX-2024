348ea24248148b33a5495bcc2f1b1d6b1790e6c2
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2024-11-30 11:27:42 -0700

    Added Database Functionality

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let app = express(); // Creating an express object
 
 let path = require("path"); // Importing the path class/library
 
-const port = 5000; // This port can change
+const port = process.env.PORT || 5000; // This port can change
 
 app.set("view engine", "ejs"); // Telling the server that our web files will be EJS files
 
@@ -14,19 +14,35 @@ app.set("views", path.join(__dirname, "views")); // Telling the server where to 
 
 app.use(express.urlencoded({extended: true})); // Allows the server to work with the requests that are submitted
 
-// const knex = require("knex") ({ // Connecting to our Postgres Database
-//     client : "pg",
-//     connection : {
-//         host : "localhost",
-//         user : "postgres",
-//         password : "admin", // This would need to change
-//         database : "mycompany",
-//         port : 5432
-//     }
-// });
+const knex = require("knex") ({ // Connecting to our Postgres Database
+    client : "pg",
+    connection : {
+        host : "localhost",
+        user : "postgres",
+        password : "Roman$EatLargeT0gas", // This would need to change
+        database : "intex",
+        port : 5432
+    }
+});
 
 app.get("/", (req, res) => {
-    res.render('index', {}) // res.send is for .html files, res.render is for .ejs files
+    knex('coding') // SQL query for collecting the data
+      .select(
+        'coding.id',
+        'coding.language_name',
+        'coding.created_year',
+        'coding.creator',
+        'coding.popularity_rank'
+      )
+      .first()
+      .then(codes => {
+        // Render the index.ejs template and pass the data
+        res.render('index', { codes });
+      })
+      .catch(error => {
+        console.error('Error querying database:', error);
+        res.status(500).send('Internal Server Error');
+      }); // res.send is for .html files, res.render is for .ejs files
 });
 
 app.listen(port, () => console.log("Express App has started and server is listening!"));
