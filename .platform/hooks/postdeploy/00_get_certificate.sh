4608f3664c85eb2128df7865afe6fb6e80e36e49
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2024-12-02 11:29:36 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 sudo certbot -n -d intexgroup2-2.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email spencerjpetty@gmail.com
+sudo certbot -n -d intex2-2.is404.net --nginx --agree-tos --email spencerjpetty@gmail.com
