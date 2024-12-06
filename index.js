ddfb735bcc439a8b4d5b26295712472eba18cbf3
sarahbboyer <166653344+sarahbboyer@users.noreply.github.com>
2024-12-05 18:34:42 -0700

    thank you

--- index.js
+++ index.js
@@ -37,13 +37,18 @@ const knex = require("knex") ({ // Connecting to our Postgres Database
     connection : {
         host : process.env.RDS_HOSTNAME || "localhost",
         user : process.env.RDS_USERNAME || "postgres",
-        password : process.env.RDS_PASSWORD || "admin", // This would need to change
+        password : process.env.RDS_PASSWORD || "admin*", // This would need to change
         // set password to admin and database to intex before committing
         database : process.env.RDS_DB_NAME || "intex",
         port : process.env.RDS_PORT || 5432,
         ssl : process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
+
+app.get('/thankYou', (req, res) => {
+  res.render('thankYou', {})
+  });
+
 
 app.get('/login', (req, res) => {
 const invalidLogin = null;
@@ -568,7 +573,7 @@ app.post('/eventRequest', (req, res) => {
   knex('event_details')
     .insert(newEventRequest)
     .then(() => {
-      res.redirect('/'); // Redirect to the homepage after successful submission
+      res.redirect('/thankYou'); // Redirect to the homepage after successful submission
     })
     .catch(error => {
       console.error('Error submitting event request:', error);
