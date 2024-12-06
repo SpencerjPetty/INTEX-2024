5af93539f5813afc2d9937d24bd228a58c13186a
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2024-12-05 20:33:03 -0700

    added dashboard

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const knex = require("knex") ({ // Connecting to our Postgres Database
     connection : {
         host : process.env.RDS_HOSTNAME || "localhost",
         user : process.env.RDS_USERNAME || "postgres",
-        password : process.env.RDS_PASSWORD || "admin*", // This would need to change
+        password : process.env.RDS_PASSWORD || "admin", // This would need to change
         // set password to admin and database to intex before committing
         database : process.env.RDS_DB_NAME || "intex",
         port : process.env.RDS_PORT || 5432,
